a6e9bcd0fd032e30e6fd4527b7966885be2e7ea6
Tiago Barbosa <sombr43@gmail.com>
2024-11-15 12:42:53 -0300

    Listar os Heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Monkey D. Luffy' }, // Capitão e primeiro membro
+  { id: 2, name: 'Roronoa Zoro' },   // Espadachim
+  { id: 3, name: 'Nami' },           // Navegadora
+  { id: 4, name: 'Usopp' },          // Atirador
+  { id: 5, name: 'Sanji' },          // Cozinheiro
+  { id: 6, name: 'Tony Tony Chopper' }, // Médico
+  { id: 7, name: 'Nico Robin' },     // Arqueóloga
+  { id: 8, name: 'Franky' },         // Carpinteiro
+  { id: 9, name: 'Brook' },          // Músico
+  { id: 10, name: 'Jinbe' }          // Timoneiro 
+]
